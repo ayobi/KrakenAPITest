5090fb251fc5723760988f60d023ff45bae753ed
Aaron O'Brien <obrien.aaron.g@gmail.com>
2020-09-05 23:20:42 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('holyyyy'));
-
 app.listen(3001, () => {
     console.log('My Kraken API');
 })
 
 const key          = 'yZQFkqCTVWdSuJ9zoHB0uUI00pjh67TjIJW6CHN0+fFnTHo5c+eo0NCY'; // API Key
 const secret       = 'HNb4JhFRBfA63UHRQmGYGdi3C8wey4c0qO/WW7aC04eL8kSGbywwf12FUiBRcDSxz5TJuqd5aLb7NTEutOEkfw=='; // API Private Key
-const otp          = 'zipzapalltheway1$';
+const otp          = 'zipzapalltheway1$'; //2fa password
 const KrakenClient = require('kraken-api');
 const kraken       = new KrakenClient(key, secret, otp);
 
